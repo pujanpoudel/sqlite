942925f503c9853cd16e6b53f70ae421292257ef
Pujan Poudel <pujanpoudelj7@gmail.com>
2025-03-02 11:44:04 +0545

    all good as of now

--- lib/features/users/data/state/user_state.dart
+++ lib/features/users/data/state/user_state.dart
@@ -4,18 +4,20 @@ class UserState {
   final bool isLoading;
   final List<User> users;
   final String? error;
+  final bool isLoaded;
 
-  UserState({
-    this.isLoading = false,
-    this.users = const [],
-    this.error,
-  });
+  UserState(
+      {this.isLoading = false,
+      this.users = const [],
+      this.error,
+      this.isLoaded = false});
 
-  UserState copyWith({bool? isLoading, List<User>? users, String? error}) {
+  UserState copyWith(
+      {bool? isLoading, List<User>? users, String? error, bool? isLoaded}) {
     return UserState(
-      isLoading: isLoading ?? this.isLoading,
-      users: users ?? this.users,
-      error: error ?? this.error,
-    );
+        isLoading: isLoading ?? this.isLoading,
+        users: users ?? this.users,
+        error: error ?? this.error,
+        isLoaded: isLoaded ?? this.isLoaded);
   }
 }
